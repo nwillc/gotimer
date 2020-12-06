a19baa3aaf581350719a9d6e60c84505eb89481e
nwillc <nwillc@gmail.com>
2020-12-06 02:08:25 -0500

    Timer go routine

--- gotimer.go
+++ gotimer.go
@@ -4,10 +4,27 @@ import (
 	"flag"
 	"fmt"
 	"github.com/nwillc/gotimer/setup"
+	"time"
 )
 
 func main() {
 	flag.Parse()
 
-	fmt.Println("Counting down:", *setup.Flags.Time)
+	duration, err := time.ParseDuration(*setup.Flags.Time)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Duration:", duration)
+	finished := make(chan bool)
+	completed := time.Now().Add(duration)
+	go func(finished chan bool) {
+		for {
+			time.Sleep(time.Second)
+			fmt.Println("-")
+			if time.Now().After(completed) {
+				finished <- true
+			}
+		}
+	}(finished)
+	<- finished
 }
--- setup/cli.go
+++ setup/cli.go
@@ -9,5 +9,5 @@ var Flags struct{
 
 func init()  {
 	Flags.Version = flag.Bool("version", false, "Display version.")
-	Flags.Time = flag.String("time", "25:00", "The time for the timer")
+	Flags.Time = flag.String("time", "5s", "The time for the timer")
 }
